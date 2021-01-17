5cd349dd13910caba69e1c23cd6a6360f18d4d2c
Amir Sajjad Poursoltani <sajjadpooresq@gmail.com>
2021-01-17 19:43:29 +0330

    add connection to DB

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 const express = require('express')
 const app = express()
 require('dotenv').config()
+app.use(express.json())
 
+const mongoose = require('mongoose');
+mongoose.connect(`mongodb://localhost/${process.env.DB_NAME}`, {useNewUrlParser: true, useUnifiedTopology: true});
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log('Connected to Database...')
+});
 
 app.listen(process.env.SERVER_PORT, () => {
     console.log(`listening on port ${process.env.SERVER_PORT}...`)
