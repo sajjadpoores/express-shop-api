52b243f4906cc516a82231325d8f364e5558b14f
Amir Sajjad Poursoltani <sajjadpooresq@gmail.com>
2021-01-17 22:14:17 +0330

    create login endpoint

--- routes/user.js
+++ routes/user.js
@@ -18,4 +18,18 @@ router.post('/register', async (req, res) => {
      res.send(newUser)
 })
 
+router.post('/login', async (req, res) => {
+     const {email, phone, password} = req.body
+     const foundUser = await UserModel.findOne({$or: [{email}, {phone}]})
+     if(foundUser) {
+          const passwordIsCorrect = await bcrypt.compare(password, foundUser.password)
+          if(passwordIsCorrect) {
+               // generate jwt token
+               return res.send('true')
+          }
+     }
+     return res.send('false')
+})
+
+
 module.exports = router
