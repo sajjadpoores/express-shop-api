cf77cc71f3d78c3398f0d9c802083ad36bacfdee
Amir Sajjad Poursoltani <sajjadpooresq@gmail.com>
2021-01-17 20:01:23 +0330

    add register route (not complete yet)

--- routes/user.js
+++ routes/user.js
@@ -3,11 +3,19 @@ const router = express.Router()
 
 const UserModel = require('../models/user')
 
+const bcrypt = require('bcrypt');
+
+async function hashPassowrd(password) {
+     const salt = await bcrypt.genSalt(10)
+     const hash = await bcrypt.hash(password, salt)
+     return hash
+}
+
 router.post('/register', async (req, res) => {
+     req.body.password = await hashPassowrd(req.body.password)
      const newUser = new UserModel(req.body)
      await newUser.save()
      res.send(newUser)
 })
-
 
 module.exports = router
