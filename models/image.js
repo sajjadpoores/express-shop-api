b1d37f3b5f006d60aa88a78f965ee66d41850de0
Amir Sajjad Poursoltani <sajjadpooresq@gmail.com>
2021-01-19 16:29:45 +0330

    add imagem odel

---
+++ models/image.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose')
+const Joi = require('joi');
+const { Schema } = mongoose
+
+const ImageModel = mongoose.model('image', new Schema ({
+    name: {
+        type: String,
+        required: true,
+        minlength: 3,
+        maxlength: 300
+    },
+    url: {
+        type: String,
+        required: true,
+        minlength: 3,
+        maxlength: 300
+    },
+    category: {
+        type: mongoose.Types.ObjectId,
+        ref: 'image-category'
+     },
+}))
+
+function validateImage(image) {
+    const JoiSchema = Joi.object({
+       name: Joi.string().min(3).max(300).required(),
+       category: Joi.objectId().required()
+    })
+    
+    return JoiSchema.validate(image)
+ }
+
+ module.exports = {
+    model: ImageModel,
+    validate: validateImage
+ }
