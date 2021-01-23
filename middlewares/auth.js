d991f9b36a21006158f643803061b77bb130ae9c
Amir Sajjad Poursoltani <sajjadpooresq@gmail.com>
2021-01-23 13:59:04 +0330

    change output format

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -4,11 +4,11 @@ require('dotenv').config()
 function auth(req, res, next) {
     const token = req.headers['x-auth']
     if(!token) {
-        return res.status(401).send('no token is provided!')
+        return res.status(401).send(['no token is provided!'])
     }
     else {
         jwt.verify(token, process.env.PASSWORD_HASH_KEY, (err, decoded) => {
-            if(err) return res.status(401).send('provided token is not verfied!')
+            if(err) return res.status(401).send(['provided token is not verfied!'])
             req.user = decoded
             next()
         })
