7c24ab1c9e7bb30db96ef91b49d95ddf22fa2aab
Amir Sajjad Poursoltani <sajjadpooresq@gmail.com>
2021-01-23 14:00:59 +0330

    add proper permission for user detail endpoint

--- routes/user.js
+++ routes/user.js
@@ -3,10 +3,11 @@ const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/user')
 const auth = require('../middlewares/auth')
+const permission = require('../middlewares/permission')
 
 router.post('/register', userController.register)
 router.post('/login', userController.login);
-router.get('/:id',[auth], userController.detail)
+router.get('/:id',[auth, permission('addUser', true)], userController.detail)
 router.get('/', userController.getAll)
 
 module.exports = router;
