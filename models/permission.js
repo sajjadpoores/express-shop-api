d5f50b099580dcd3d323fcf2729be31e058161b5
Amir Sajjad Poursoltani <sajjadpooresq@gmail.com>
2021-01-25 16:05:18 +0330

    add randomString function and add addPayment permission to permission model

--- models/permission.js
+++ models/permission.js
@@ -1,107 +1,116 @@
-const Joi = require('joi');
-const mongoose = require('mongoose')
-const { Schema } = mongoose
-const PermissionModel = mongoose.model('permission', new Schema ({
-   name: {
+const Joi = require("joi");
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+const PermissionModel = mongoose.model(
+  "permission",
+  new Schema({
+    name: {
       type: String,
       required: true,
       minlength: 3,
-      maxlength: 300
-   },
-   writeArticle: {
+      maxlength: 300,
+    },
+    writeArticle: {
       type: Boolean,
       required: true,
-      default: false
-   },
-   writeArticleComment: {
+      default: false,
+    },
+    writeArticleComment: {
       type: Boolean,
       required: true,
-      default: true
-   },
-   writeBlog: {
+      default: true,
+    },
+    writeBlog: {
       type: Boolean,
       required: true,
-      default: false
-   },
-   writeBlogComment: {
+      default: false,
+    },
+    writeBlogComment: {
       type: Boolean,
       required: true,
-      default: true
-   },
-   addProduct: {
+      default: true,
+    },
+    addProduct: {
       type: Boolean,
       required: true,
-      default: false
-   },
-   addCategory: {
+      default: false,
+    },
+    addCategory: {
       type: Boolean,
       required: true,
-      default: false
-   },
-   addDiscount: {
+      default: false,
+    },
+    addDiscount: {
       type: Boolean,
       required: true,
-      default: false
-   },
-   addProperty: {
+      default: false,
+    },
+    addProperty: {
       type: Boolean,
       required: true,
-      default: false
-   },
-   addConfig: {
+      default: false,
+    },
+    addConfig: {
       type: Boolean,
       required: true,
-      default: false
-   },
-   addFaq: {
+      default: false,
+    },
+    addFaq: {
       type: Boolean,
       required: true,
-      default: false
-   },
-   addImage: {
+      default: false,
+    },
+    addImage: {
       type: Boolean,
       required: true,
-      default: false
-   },
-   addPermission: {
+      default: false,
+    },
+    addPermission: {
       type: Boolean,
       required: true,
-      default: false
-   },
-   addTicket: {
+      default: false,
+    },
+    addTicket: {
       type: Boolean,
       required: true,
-      default: false
-   },
-   addUser: {
+      default: false,
+    },
+    addUser: {
       type: Boolean,
       required: true,
-      default: false
-   }
-}))
+      default: false,
+    },
+    addPayment: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
+  })
+);
 
 function validatePermission(permission) {
-   const JoiSchema = Joi.object({
-      name: Joi.string().min(3).max(300).required(),
-      writeArticle: Joi.boolean(),
-      writeArticleComment: Joi.boolean(),
-      writeBlog: Joi.boolean(),
-      writeBlogComment: Joi.boolean(),
-      addProduct: Joi.boolean(),
-      addCategory: Joi.boolean(),
-      addDiscount: Joi.boolean(),
-      addProperty: Joi.boolean(),
-      addConfig: Joi.boolean(),
-      addFaq: Joi.boolean(),
-      addImage: Joi.boolean(),
-      addPermission: Joi.boolean(),
-      addTicket: Joi.boolean(),
-      addUser: Joi.boolean()
-   })
-   return JoiSchema.validate(permission)
+  const JoiSchema = Joi.object({
+    name: Joi.string().min(3).max(300).required(),
+    writeArticle: Joi.boolean(),
+    writeArticleComment: Joi.boolean(),
+    writeBlog: Joi.boolean(),
+    writeBlogComment: Joi.boolean(),
+    addProduct: Joi.boolean(),
+    addCategory: Joi.boolean(),
+    addDiscount: Joi.boolean(),
+    addProperty: Joi.boolean(),
+    addConfig: Joi.boolean(),
+    addFaq: Joi.boolean(),
+    addImage: Joi.boolean(),
+    addPermission: Joi.boolean(),
+    addTicket: Joi.boolean(),
+    addUser: Joi.boolean(),
+    addPayment: Joi.boolean(),
+  });
+  return JoiSchema.validate(permission);
 }
 
 module.exports = {
-   model: PermissionModel,
-   validate: validatePermission
-}
+  model: PermissionModel,
+  validate: validatePermission,
+};
--- utilities/helpers.js
+++ utilities/helpers.js
@@ -1,28 +1,40 @@
-const {model: PermissionModel} = require('../models/permission')
+const { model: PermissionModel } = require("../models/permission");
 async function createDefaultPermission() {
-    const normalUserPermission = new PermissionModel({ name: 'normal user' });
-    await normalUserPermission.save();
-    return normalUserPermission
+  const normalUserPermission = new PermissionModel({ name: "normal user" });
+  await normalUserPermission.save();
+  return normalUserPermission;
 }
 
 function removeFieldFromDocument(document, fields) {
-    const documentJson = document.toObject()
-    fields.forEach(field => {
-        delete documentJson[field]
-    })
-    return documentJson
+  const documentJson = document.toObject();
+  fields.forEach((field) => {
+    delete documentJson[field];
+  });
+  return documentJson;
 }
 
 function extractFieldsFromObject(obj, fields) {
-    let outputObject = {}
-    fields.forEach(field => {
-        outputObject[field] = obj[field]
-    })
-    return outputObject
+  let outputObject = {};
+  fields.forEach((field) => {
+    outputObject[field] = obj[field];
+  });
+  return outputObject;
+}
+
+function randomString(length) {
+  var result = "";
+  var characters =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  var charactersLength = characters.length;
+  for (var i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return result;
 }
 
 module.exports = {
-    createDefaultPermission,
-    removeFieldFromDocument,
-    extractFieldsFromObject
-}
+  createDefaultPermission,
+  removeFieldFromDocument,
+  extractFieldsFromObject,
+  randomString
+};
